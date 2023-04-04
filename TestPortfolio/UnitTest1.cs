2d5a21402a59e669fe3fca1d97537be9a0a6058c
AbhilashGurrala <125271237+AbhilashGurrala@users.noreply.github.com>
2023-04-05 00:17:19 +0530

    Modified the names of unit tests and used a different approach to check null conditions.

--- TestPortfolio/UnitTest1.cs
+++ TestPortfolio/UnitTest1.cs
@@ -4,78 +4,105 @@ namespace TestPortfolio
 {
     public class UnitTest1
     {
+
         //A unit test for the method ListAllInvestments() checking if the method returns a null value
         [Fact]
-        public void ListAllInvestments_Test()
+        public void ListAllInvestments_CheckNullValue_Test()
         {
             //Arrange
             Portfolio.PortfolioManager portfolioManager = new Portfolio.PortfolioManager();
-            var result = portfolioManager.ListAllInvestements();
             //ACT
-            if (result == null)
+            var result = portfolioManager.ListAllInvestements();
+      
+            if (String.IsNullOrEmpty(result))
             {
                 //ASSERT
-                Assert.Fail("IS NULL");
+                Assert.False(true);
             }
-
         }
+
+    
+ 
         //A unit test for the method ListPortfolioAssetsByName() checking if the method returns a null value
         [Fact]
-        public void ListPortfolioAssetsByName_Test()
+        public void ListPortfolioAssetsByName_CheckNullValue_Test()
         {
             //Arrange
             Portfolio.PortfolioManager portfolioManager = new Portfolio.PortfolioManager();
             List<string> testAssetNames = new List<string>(); //creating a list of string and adding few asset names for testing
             testAssetNames.Add("MSFT");
             testAssetNames.Add("AAPL");
-            var result = portfolioManager.ListPortfolioAssetsByName(testAssetNames); //assigning the returned string to a variable result
             //ACT
-            if (result == null)
+            var result = portfolioManager.ListPortfolioAssetsByName(testAssetNames); //assigning the returned string to a variable result
+    
+            if (String.IsNullOrEmpty(result))
             {
                 //ASSERT
-                Assert.Fail("IS NULL"); //fail the test immediately
+                Assert.False(true);
             }
 
         }
+
+        //A unit test for the method ListPortfolioAssetsByName() checking if there is an invalid asset name
+        [Fact]
+        public void ListPortfolioAssetsByName_InvalidAssetName_Test()
+        {
+            //Arrange
+            Portfolio.PortfolioManager portfolioManager = new Portfolio.PortfolioManager();
+            List<string> testAssetNames = new List<string>(); //creating a list of string and adding few asset names for testing
+            testAssetNames.Add("MSFT");
+            testAssetNames.Add("AAPL");
+            //ACT
+            var result = portfolioManager.ListPortfolioAssetsByName(testAssetNames); //assigning the returned string to a variable result
+
+            if (String.IsNullOrEmpty(result))
+            {
+                //ASSERT
+                Assert.False(true);
+            }
+
+        }
+
+
         //A unit test case for the method ListPortfolioSalesInRange() when DateTime given is valid
         //also checks if the meethod returns a null value
         [Fact]
-        public void ListPortfolioSalesInRange_ValidRange_Test()
+        public void ListPortfolioSalesInRange_ValidRangeNullCheck_Test()
         {
             //Arrange
             Portfolio.PortfolioManager portfolioManager = new Portfolio.PortfolioManager();
             DateTime startDateTime = Convert.ToDateTime("03-03-2023 02:39:22");
             DateTime endDateTime = DateTime.Now;
-            var result = portfolioManager.ListPortfolioSalesInRange(startDateTime, endDateTime); //assigning the returned string to a variable result
             //ACT
-            if (result == null)
+            var result = portfolioManager.ListPortfolioSalesInRange(startDateTime, endDateTime); //assigning the returned string to a variable result
+            if (String.IsNullOrEmpty(result))
             {
                 //ASSERT
-                Assert.Fail("IS NULL");
+                Assert.False(true);
             }
 
         }
         //A unit test case for the method ListPortfolioSalesInRange() when DateTime given is invalid,
         //also checks if the meethod returns a null value
         [Fact]
-        public void ListPortfolioSalesInRange_InvalidRange_Test()
+        public void ListPortfolioSalesInRange_InvalidRange_FailTest()
         {
             //Arrange
             Portfolio.PortfolioManager portfolioManager = new Portfolio.PortfolioManager();
             DateTime startDateTime = Convert.ToDateTime("03-03-2023 02:39:22");
             DateTime endDateTime = Convert.ToDateTime("02-01-2023 03:40:47");
             //ACT
-            if (endDateTime < startDateTime || startDateTime > endDateTime) //checking if start or end datetime is invalid
+            if (endDateTime < startDateTime) //checking if start or end datetime is invalid
             {
                 //ASSERT
                 Assert.Fail("Invalid date time range");
             }
             var result = portfolioManager.ListPortfolioSalesInRange(startDateTime, endDateTime); //assigning the returned string to a variable result
             //ACT
-            if (result == null)
+            if (String.IsNullOrEmpty(result))
             {
                 //ASSERT
-                Assert.Fail("IS NULL");
+                Assert.False(true);
             }
 
         }
